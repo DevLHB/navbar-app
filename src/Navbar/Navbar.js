38334cab32179b9ec5454361663b75c9c23d07d3
yassine lahbib <yassine.lahbib@esprit.tn>
2021-10-23 11:03:15 +0100

    first commit

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="Navbar">
+      <span className="nav-logo">DevLHB</span>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/service">Service</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
